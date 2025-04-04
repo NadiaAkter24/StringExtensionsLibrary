e536d57a72cdb6888f73d8e721c9862f6db4a2b1
NadiaAkter24 <nnadia3@my.centennialcollege.ca>
2025-04-03 20:32:12 -0400

    Add project files.

---
+++ StringExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringExtensionsLibrary
+{
+    public static class StringExtensions
+    {
+        public static bool StartsWithUpperCase(this string str)
+        {
+            if (string.IsNullOrEmpty(str))
+                return false;
+
+            return char.IsUpper(str[0]);
+        }
+    }
+}
